e542b1daaa32bf8e2cc3bce030a09511b504108b
Thomas Hobohm <public@thomashobohm.com>
2019-03-15 12:43:15 -0500

    Fixed unnecessary h1 transitions in Javascript.

--- js/index.js
+++ js/index.js
@@ -2,11 +2,9 @@ $(document).ready(() => {
   $(".red").hover(() => {
     $(".header-container").css("background", "#DB3340");
     $(".red").css("color", "#fff");
-    $("h1").css("color", "#fff");
   }, () => {
     $(".header-container").css("background", "#fff");
     $(".red").css("color", "#DB3340");
-    $("h1").css("color", "#28abe3");
   });
 
   $(".blue").hover(() => {
@@ -22,20 +20,16 @@ $(document).ready(() => {
   $(".green").hover(() => {
     $(".header-container").css("background", "#1FDA9A");
     $(".green").css("color", "#fff");
-    $("h1").css("color", "#fff");
   }, () => {
     $(".header-container").css("background", "#fff");
     $(".green").css("color", "#1FDA9A");
-    $("h1").css("color", "#28abe3");
   });
 
   $(".yellow").hover(() => {
     $(".header-container").css("background", "#e8b71a");
     $(".yellow").css("color", "#fff");
-    $("h1").css("color", "#fff");
   }, () => {
     $(".header-container").css("background", "#fff");
     $(".yellow").css("color", "#e8b71a");
-    $("h1").css("color", "#28abe3");
   });
 });
